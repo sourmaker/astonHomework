ddd655321821aebee7282143055a7fcd0485e09c
Андрей Устюжанинов <ptvpfan@gmail.com>
2026-06-19 16:43:14 +0300

    bug fixes

--- src/main/java/ru/aston/hometask/Student.java
+++ src/main/java/ru/aston/hometask/Student.java
@@ -1,14 +1,27 @@
-package project;
+package ru.aston.hometask;
+
 
 public class Student {
+
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     Student(String name) {
         this.name = name;
     }
+
 
     @Override
     public String toString() {
         return name;
     }
 }
+
+
